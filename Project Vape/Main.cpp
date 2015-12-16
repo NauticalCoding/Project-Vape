56f90a92cf13a83d53a0ce42fa5991df978e99bd
NauticalCoding <lordofhens@gmail.com>
2015-12-16 05:50:50 -0500

    Added Aimbot + Triggerbot bug fix
    Triggerbot was trying to shoot so fast it stopped simulating clicks
    properly.
    Easy fix by adding a delay of 25 ms.

--- Project Vape/Main.cpp
+++ Project Vape/Main.cpp
@@ -9,14 +9,16 @@
 
 	OFFSETS 
 
-	LocalPlayer:	"client.dll"	+ 0x006CC00C
+	Localplayer:	"client.dll"	+ 0x006CC00C
 	Playerlist:		"client.dll"	+ 0x0062B104
-	CrosshairID:    LocalPlayer		+ 0x2C40
+	CrosshairID:    Localplayer		+ 0x2C40
 
 	Playercount:    "client.dll"	+ 0x60ABE4
 
 	Viewmatrix: "engine.dll" + 0x5D5C54
 	Antiflick = Viewmatrix + 0x4C
+
+	Eyeangles:    "engine.dll" + 0x4AB2A4
 
 	PlayerStruct offsets
 
@@ -26,7 +28,6 @@
 */
 
 #include "WindowProcess.h"
-#include <Windows.h>
 #include <string>
 
 // Structures
@@ -38,13 +39,19 @@ struct Vector {
 	float z;
 };
 
+struct Angle {
+
+	float p;
+	float y;
+	float r;
+};
+
 struct Player {
 
 	int health;
 	int team;
 	int mFlags;
-	int isDormant;
-	
+
 	Vector origin;
 
 	void ReadMem(HANDLE procHandle,DWORD addr) {
@@ -52,7 +59,6 @@ struct Player {
 		ReadProcessMemory(procHandle, (DWORD*)(addr + 0x90), &health, sizeof(int), NULL); // read hp
 		ReadProcessMemory(procHandle, (DWORD*)(addr + 0x9C), &team, sizeof(int), NULL); // read team
 		ReadProcessMemory(procHandle, (DWORD*)(addr + 0x260), &origin, sizeof(Vector), NULL); // read origin
-		ReadProcessMemory(procHandle, (DWORD*)(addr + 0x164), &isDormant, sizeof(bool), NULL); // read isDormant
 	}
 }myPlayer,plyList[64];
 
@@ -61,15 +67,228 @@ struct ViewMatrix {
 	float A[4][4];
 }worldToScreen;
 
+
 // Global vars
 
 WindProc game;
-int playerCount = 0;
-HPEN boxPen = CreatePen(PS_SOLID, 2, RGB(0, 255, 100));
-HBRUSH hpBar = CreateSolidBrush(RGB(255, 75, 75));
+int playerCount			= 64;
+HPEN boxPen				= CreatePen(PS_SOLID, 2, RGB(0, 255, 100));
+HBRUSH hpBar			= CreateSolidBrush(RGB(255, 75, 75));
 
-// Code
+int triggerShootDelay	= 25; // 25 ticks or milliseconds
+int lastShot			= 0;
 
+bool visualsOn			= false;
+bool triggerbotOn		= false;
+DWORD aimbotKey			= NULL;
+bool aimbotOn			= false;
+
+bool bindPressed		= false;
+int bindPressedTick		= 0; // when was the bind pressed?
+
+
+// Code - Other
+float Distance(Vector start, Vector end)
+{
+	float deltX = end.x - start.x;
+	float deltY = end.y - start.y;
+	float deltZ = end.z - start.z;
+
+	return (sqrt(pow(deltX, 2) + pow(deltY, 2) + pow(deltZ, 2)));
+}
+
+float Distance2D(Vector start, Vector end)
+{
+	float deltX = end.x - start.x;
+	float deltY = end.y - start.y;
+
+	return (sqrt(pow(deltX, 2) + pow(deltY, 2)));
+}
+
+Angle VecToAngle(Vector start, Vector end)
+{
+
+	Angle answer;
+
+	end.z = end.z + 5.0f;
+
+	float deltX = end.x - start.x;
+	float deltY = end.y - start.y;
+	float deltZ = end.z - start.z;
+
+	float dist3D = Distance(start, end);
+	float dist2D = Distance2D(start, end);
+
+	answer.p = asinf(deltZ / dist3D) * (180.0f / 3.14f);
+	answer.y = -asinf(deltY / dist2D) * (180.0f / 3.14f);
+	answer.r = 0;
+
+	if (deltX > 0) {
+
+		answer.y = 180.0f - answer.y;
+	}
+
+	if (answer.y > 180.0f) {
+
+		answer.y = -(360.0f - answer.y);
+	}
+
+	return answer;
+}
+
+// Code - Keybinds
+
+void RunKeyBinds()
+{
+
+	if (aimbotKey == NULL) { // we need to choose an aimbot key..
+
+		for (DWORD i = 0x0; i < 0x80; i += 0x1) { // loop throuh all keys
+
+			if (GetAsyncKeyState(i) != 0) { // this is our key
+
+				aimbotKey = i;
+				printf("Aimbot binded to key: 0x%x\n", aimbotKey);
+				break;
+			}
+		}
+
+		return;
+	}
+
+	// aimbot
+	if (GetAsyncKeyState(aimbotKey) != 0) {
+
+		aimbotOn = true;
+	}
+	else {
+
+		aimbotOn = false;
+	}
+
+	// reset 
+	if (bindPressed && bindPressedTick < GetTickCount() - 500) {
+
+		bindPressed = false;
+	}
+
+	// esp
+	if (GetAsyncKeyState(VK_NUMPAD0) != 0 && !bindPressed) {
+
+		bindPressed = true;
+		bindPressedTick = GetTickCount();
+
+		visualsOn = !visualsOn;
+	}
+
+	// triggerbot
+	if (GetAsyncKeyState(VK_NUMPAD1) != 0 && !bindPressed) {
+
+		bindPressed = true;
+		bindPressedTick = GetTickCount();
+
+		triggerbotOn = !triggerbotOn;
+	}
+
+}
+
+// Code - Aim
+void RunAim(DWORD engine)
+{
+
+	if (!aimbotOn) {
+
+		return;
+	}
+
+	float angDiff = 0;
+	Angle eyeAngles;
+	Player targ;
+	targ.health = -1337;
+
+	// read eye angles
+	ReadProcessMemory(game.procHandle, (DWORD*)(engine + 0x4AB2A4), &eyeAngles, sizeof(Angle), NULL);
+
+	for (int i = 0; i < playerCount; i++) {
+
+		if (plyList[i].health <= 1) { // they're dead
+
+			continue;
+		}
+
+		if (Distance(myPlayer.origin, plyList[i].origin) < 2) { // this is localplayer
+
+			continue;
+		}
+
+		if (plyList[i].team == 1002) { // this is a spectator
+
+			continue;
+		}
+
+		Angle tempAng = VecToAngle(plyList[i].origin, myPlayer.origin);
+
+		float tempAngDiff = abs(tempAng.y - eyeAngles.y) + abs(tempAng.p - eyeAngles.p);
+
+		if (targ.health == -1337 || tempAngDiff < angDiff) {
+
+			targ = plyList[i];
+			angDiff = tempAngDiff;
+		}
+	}
+
+	if (targ.health == -1337) { // no target found
+
+		return;
+	}
+
+	Angle angToTarg = VecToAngle(targ.origin, myPlayer.origin);
+	WriteProcessMemory(game.procHandle, (DWORD*)(engine + 0x4AB2A4), &angToTarg, sizeof(Angle), NULL);
+}
+
+// Code - Triggerbot
+void triggerBot()
+{
+	if (lastShot < GetTickCount() - triggerShootDelay) { // this fixes a bug where the triggerbot tries to shoot too fast and fails
+
+		INPUT input;
+
+		// left click press
+		input.type = INPUT_MOUSE;
+		input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+		SendInput(1, &input, sizeof(INPUT));
+
+		ZeroMemory(&input, sizeof(INPUT)); // fill the memory address of 'input' with zeros
+
+		// left click release
+		input.type = INPUT_MOUSE;
+		input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+		SendInput(1, &input, sizeof(INPUT));
+
+		lastShot = GetTickCount();
+	}
+}
+
+bool canTriggerBot(DWORD localplyAddr) {
+
+	if (!triggerbotOn) { // if triggerbot isnt toggled..
+
+		return false;
+	}
+
+	// read the CrosshairID
+	int crosshairID;
+	ReadProcessMemory(game.procHandle, (DWORD*)((localplyAddr + 0x2C40)), &crosshairID, sizeof(int), NULL);
+
+	if (crosshairID == 0 || myPlayer.health <= 0 || crosshairID > playerCount) 
+	{ 
+		return false; // crosshairID: 0 = world, crosshairID > playercount = invalid target
+	} 
+
+	return true;
+}
+
+// Code - Drawing
 bool WorldToScreenConvert(Vector from, float * to)
 {
 
@@ -105,13 +324,13 @@ bool WorldToScreenConvert(Vector from, float * to)
 	return true;
 }
 
-float Distance(Vector start, Vector end)
+void DrawOutlinedText(HDC hdc, int x, int y, std::string text, COLORREF col)
 {
-	float deltX = end.x - start.x;
-	float deltY = end.y - start.y;
-	float deltZ = end.z - start.z;
 
-	return (sqrt(pow(deltX, 2) + pow(deltY, 2) + pow(deltZ, 2)));
+	SetTextColor(hdc, RGB(0, 0, 0));
+	TextOutA(hdc, x - 1, y - 1, text.c_str(), text.length());
+	SetTextColor(hdc, col);
+	TextOutA(hdc, x, y, text.c_str(), text.length());
 }
 
 LRESULT CALLBACK OverlayCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -130,73 +349,89 @@ LRESULT CALLBACK OverlayCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 
 		FillRect(memHDC, &game.bitMapRect, WHITE_BRUSH); // make memory dc see transparent
 
-		SetBkMode(memHDC, TRANSPARENT);
-		SetTextAlign(memHDC, TA_CENTER);
+		if (visualsOn) {
 
-		SelectObject(memHDC, boxPen);
+			DrawOutlinedText(memHDC, 100, 15, std::string("Visuals: On"), RGB(0, 255, 0));
+		}
+		else {
 
-		for (int i = 0; i < playerCount; i++) {
+			DrawOutlinedText(memHDC, 100, 15, std::string("Visuals: Off"), RGB(255, 0, 0));
+		}
 
-			if (plyList[i].isDormant) {
+		if (triggerbotOn) {
 
-				continue;
+			DrawOutlinedText(memHDC, 100, 35, std::string("Triggerbot: On"), RGB(0, 255, 0));
+		}
+		else {
+
+			DrawOutlinedText(memHDC, 100, 35, std::string("Triggerbot: Off"), RGB(255, 0, 0));
+		}
+
+		if (aimbotOn) {
+
+			DrawOutlinedText(memHDC, 100, 55, std::string("Aimbot: On"), RGB(0, 255, 0));
+		}
+		else {
+
+			DrawOutlinedText(memHDC, 100, 55, std::string("Aimbot: Off"), RGB(255, 0, 0));
+		}
+
+		if (visualsOn) {
+			SetBkMode(memHDC, TRANSPARENT);
+			SetTextAlign(memHDC, TA_CENTER);
+
+			SelectObject(memHDC, boxPen);
+
+			for (int i = 0; i < playerCount; i++) {
+
+				if (plyList[i].team == 1002) {
+
+					continue;
+				}
+
+				if (plyList[i].health <= 1) {
+
+					continue;
+				}
+
+				float dist = Distance(plyList[i].origin, myPlayer.origin);
+
+				if (dist < 1.0f || dist > 5000.0f) { // this is localplayer
+
+					continue;
+				}
+
+				float * scrnPos = new float[2];
+
+				if (WorldToScreenConvert(plyList[i].origin, scrnPos)) { // if they're on screen the do the works....
+
+					// calc box size
+					int width = 20000 / dist;
+					int height = 50000 / dist;
+
+					// draw box + hp bar
+					Rectangle(memHDC, scrnPos[0] - width, scrnPos[1] - height, scrnPos[0] + width, scrnPos[1]);
+					Rectangle(memHDC, scrnPos[0] - width - width / 3, scrnPos[1] - height, scrnPos[0] - width, scrnPos[1]);
+
+					SelectObject(memHDC, hpBar);
+
+					float frac = plyList[i].health / 100.0f;
+
+					Rectangle(memHDC, scrnPos[0] - width - width / 3, scrnPos[1] - height * frac, scrnPos[0] - width, scrnPos[1]);
+					SelectObject(memHDC, GetStockObject(HOLLOW_BRUSH));
+
+					// draw dist info
+
+					std::string distStr = std::string("Dist: ");
+					distStr.append(std::to_string((int)dist));
+
+					// Outlined text
+					DrawOutlinedText(memHDC, scrnPos[0], scrnPos[1] - height - 20, distStr, RGB(0, 255, 100));
+
+				}
+
+				delete[] scrnPos;
 			}
-
-			if (plyList[i].team == 1002) { // spectator team
-
-				continue;
-			}
-
-			if (plyList[i].health <= 1) {
-
-				continue;
-			}
-
-			float dist = Distance(plyList[i].origin, myPlayer.origin);
-
-			if (dist < 1.0f) { // this is localplayer
-
-				continue;
-			}
-
-			float *scrnPos = new float[2];
-
-			if (WorldToScreenConvert(plyList[i].origin, scrnPos)) { // if they're on screen then do the works....
-
-				// calc box size
-				int width = 20000 / dist;
-				int height = 50000 / dist;
-
-				// draw box + hp bar
-				Rectangle(memHDC, scrnPos[0] - width, scrnPos[1] - height, scrnPos[0] + width, scrnPos[1]);
-				Rectangle(memHDC, scrnPos[0] - width - width / 3, scrnPos[1] - height, scrnPos[0] - width, scrnPos[1]);
-
-				SelectObject(memHDC, hpBar);
-
-				float frac = plyList[i].health / 100.0f;
-
-				Rectangle(memHDC, scrnPos[0] - width - width / 3, scrnPos[1] - height * frac, scrnPos[0] - width, scrnPos[1]);
-				SelectObject(memHDC, GetStockObject(HOLLOW_BRUSH));
-				
-				// draw dist info
-
-				std::string distStr = std::string("Dist: ");
-				distStr.append(std::to_string((int)dist));
-
-				std::string teamStr = std::string("Team: ");
-				teamStr.append(std::to_string((int)plyList[i].team));
-
-
-				// Outlined text
-				SetTextColor(memHDC, RGB(0, 0, 0));
-				TextOutA(memHDC, scrnPos[0] + 1, scrnPos[1] - height - 19, distStr.c_str(), distStr.length());
-				SetTextColor(memHDC, RGB(0, 255, 100));
-				TextOutA(memHDC, scrnPos[0], scrnPos[1] - height - 20, distStr.c_str(), distStr.length());
-
-				TextOutA(memHDC, scrnPos[0], scrnPos[1] - height - 40, teamStr.c_str(), teamStr.length());
-			}
-
-			delete[] scrnPos;
 		}
 
 		// Copy dat shit
@@ -211,7 +446,7 @@ LRESULT CALLBACK OverlayCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 
 	case WM_ERASEBKGND:
 	{
-		InvalidateRect(hwnd, NULL, FALSE);
+		InvalidateRect(hwnd, NULL, false);
 		return 1;
 	}
 
@@ -239,41 +474,14 @@ LRESULT CALLBACK OverlayCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	return 0;
 }
 
-void triggerBot()
-{
-	INPUT input;
-
-	// left click press
-	input.type = INPUT_MOUSE;
-	input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-	SendInput(1, &input, sizeof(INPUT));
-
-	ZeroMemory(&input, sizeof(INPUT)); // fill the memory address of 'input' with zeros
-
-	// left click release
-	input.type = INPUT_MOUSE;
-	input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-	SendInput(1, &input, sizeof(INPUT));
-}
-
-bool canTriggerBot(DWORD localplyAddr) {
-
-	// read the CrosshairID
-	int crosshairID;
-	ReadProcessMemory(game.procHandle, (DWORD*)((localplyAddr + 0x2C40)), &crosshairID, sizeof(int), NULL);
-	
-	if (crosshairID == 0 || myPlayer.health <= 0) { return false; } // crosshairID: 0 = world
-
-	return true;
-}
-
+// Code - Main
 int main()
 {
 
-	printf("Startup..\n");
+	printf("Loading Project Vapor by Nautical & Fisheater.");
 
 	while (!game.AttachToWindow("Garry's Mod")) {
-
+		
 		Sleep(1000);
 	}
 
@@ -284,7 +492,7 @@ int main()
 
 	game.CreateModuleSnapshot();
 
-	HWND overlay = game.PrepOverlay("Project Vapor", OverlayCallback);
+	HWND overlay = game.PrepOverlay("Project Vapor by Nautical & Fisheater", OverlayCallback);
 
 	MODULEENTRY32 client = game.GetModuleByName("client.dll");
 	MODULEENTRY32 engine = game.GetModuleByName("engine.dll");
@@ -294,6 +502,9 @@ int main()
 	
 	int antiflick = 1;
 	
+	Sleep(1000);
+	printf("\nPlease press a key to bind your aimbot. \n");
+
 	while (true) {
 
 		// Read antiflick for view matrix
@@ -305,14 +516,14 @@ int main()
 			ReadProcessMemory(game.procHandle, (DWORD*)(engine.modBaseAddr + 0x5D5C54), &worldToScreen, sizeof(ViewMatrix), NULL);
 		}
 
-		// Grab ply count
-		ReadProcessMemory(game.procHandle, (DWORD*)(client.modBaseAddr + 0x60ABE4), &playerCount, sizeof(int), NULL);
-
 		// Read localply
 		myPlayer.ReadMem(game.procHandle, localplyAddr);
 
 		// Player list loop
 		for (int i = 0; i < playerCount; i++) {
+
+			// clean the structure of old data
+			plyList[i] = Player();
 
 			DWORD plyAddr;
 			ReadProcessMemory(game.procHandle, (DWORD*)((client.modBaseAddr + 0x0062B104) + 0x10 * i), &plyAddr, sizeof(DWORD), NULL);
@@ -329,14 +540,21 @@ int main()
 			DispatchMessage(&game.overlayMsg);
 		}
 
-		// Triggerbot
-		if (canTriggerBot(localplyAddr)) {
-			triggerBot();
+		// bind loop
+		RunKeyBinds();
+
+		// aimbot
+		RunAim((DWORD)engine.modBaseAddr);
+
+		// trigger bot
+		if (canTriggerBot(localplyAddr)) { // check if we should shoot..
+
+			triggerBot(); // simulate a click
 		}
 	}
 	
-	system("pause");
 	game.Detatch();
+	system("pause");
 	return 0;
 }
 
